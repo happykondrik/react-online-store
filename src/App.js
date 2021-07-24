5b15b0c3836549e4e582f009f84d404894b4db9b
Aleksey Kondratyev <aleksey.kondratyev7@gmail.com>
2021-07-24 21:55:07 +0300

    next commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Cart</h2>
+
+          <div className="items">
+
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/computers/1.png)' }} className="cartItemImg"></div>
+              <div className="cartItemText">
+                <p>MacBook Air 13-inch</p>
+                <b>199 usd</b>
+              </div>
+              <img className="removeButton" src="/img/button-remove.svg" alt="Remove" />
+            </div>
+
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/computers/1.png)' }} className="cartItemImg"></div>
+              <div className="cartItemText">
+                <p>MacBook Air 13-inch</p>
+                <b>199 usd</b>
+              </div>
+              <img className="removeButton" src="/img/button-remove.svg" alt="Remove" />
+            </div>
+
+          </div>
+
+          <ul className="cartTotalBlock">
+            <li>
+              <span>Total:</span>
+              <div></div>
+              <b>199.00 usd</b>
+            </li>
+            <li>
+              <span>Tax 5%:</span>
+              <div></div>
+              <b>9.95 usd</b>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="/img/logo.svg" />
@@ -13,7 +55,7 @@ function App() {
         <ul className="headerRight">
           <li className="cart">
             <img width={18} height={18} src="/img/cart.svg" />
-            <span>102 usd</span>
+            <span>199 usd</span>
           </li>
           <li className="user">
             <img width={18} height={18} src="/img/user.svg" />
@@ -22,14 +64,21 @@ function App() {
       </header>
 
       <div className="content">
-        <h1>
-          Computer catalog
-        </h1>
+        <div className="search">
+          <h1>Computer catalog</h1>
+          <div className="search-block">
+            <img width={14} height={14} src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." />
+          </div>
+        </div>
 
         <div className="computers">
 
-          <div className="card" >
-            <img width={140} height={120} src="/img/computers/1.jpg" alt="Computers" />
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
+            <img width={140} height={120} src="/img/computers/1.png" alt="Computers" />
             <h5> MacBook Air 13-inch</h5>
             <div className="cardBottom">
               <div className="price">
@@ -51,7 +100,7 @@ function App() {
                 <b>199 usd</b>
               </div>
               <button className="button">
-              <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
+                <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
@@ -65,7 +114,7 @@ function App() {
                 <b>199 usd</b>
               </div>
               <button className="button">
-              <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
+                <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
@@ -79,7 +128,7 @@ function App() {
                 <b>199 usd</b>
               </div>
               <button className="button">
-              <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
+                <img width={25} height={25} src="/img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
@@ -87,7 +136,7 @@ function App() {
         </div>
       </div>
     </div>
-      );
+  );
 }
 
-      export default App;
+export default App;
